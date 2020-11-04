1d557f2480448d5e2059527e327545165420d6ec
wastech <fataiwasiu2@gmail.com>
2020-11-04 09:29:55 +0100

    created hero  component

---
+++ client/src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+ <!-- <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>-->
+</div>
+  </div>
+</template>
+<style scoped>
+.jumbotron{
+  border-radius: 3em;
+  height: 25em;
+  background-color: #ffc93c;
+  color: white;
+}
+.display-4{
+  text-align: center;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="home">
-   
+  <div class="container">
+   <Hero/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
+import Hero from '@/components/Hero.vue'
 
 export default {
   name: 'Home',
   components: {
    // HelloWorld
+   Hero
   }
 }
 </script>
+<style scoped>
+.container{
+  max-width: 95%;
+  width: 100%;
+}
+</style>
