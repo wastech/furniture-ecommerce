f674fa944f07563485e7d2ff62e064e69189cad1
wastech <fataiwasiu2@gmail.com>
2020-11-04 09:11:08 +0100

    created navbar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+   <appNavbar/>
+     <router-view/>
   </div>
-  <router-view/>
+ 
 </template>
+<script>
+import appNavbar from '@/components/appNavbar'
+export default {
+  components:{
+    appNavbar
+  }
+}
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  background-color: white;
 }
 
 #nav {
---
+++ client/src/components/appNavbar.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="hello">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <a class="navbar-brand">
+      <router-link to="/">Our Products</router-link>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav   mr-auto ">
+        <li class="nav-item">
+          <a class="nav-link">
+            <router-link to="/">Chairs</router-link>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/">Sofas</router-link>
+          </a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/">Beds</router-link>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/">Tables</router-link>
+          </a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="enter you email...." aria-label="Search">
+        <button class="btn btn-primary my-2 my-sm-0 " type="submit">search</button>
+      </form>
+    </div>
+  </nav>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'appnavbar',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+ul {
+  margin: 0 auto;
+  color: black;
+  font-weight: bolder;
+  padding: 2rem;
+}
+button {
+  border-radius: 0.7rem;
+}
+input {
+  border: 0.2rem solid #ccc;
+  border-radius: 0.7rem;
+}
+#nav a.router-link-exact-active {
+    color: black;
+    font-size: 1.5em;
+}
+</style>
