c8fec92c37229afa6ede86c5488d77c325f6c6c1
wastech <fataiwasiu2@gmail.com>
2020-11-09 21:59:16 +0100

    set up the vuex

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,34 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    cart: JSON.parse(localStorage.getItem("cart")) || [],
+    charge: {},
+  },
+  getters: {
+    getCart: (state) => state.cart,
+    getCharge: (state) => state.charge,
   },
   mutations: {
+    setCart: (state, payload) => {
+      state.cart.push(payload);
+    },
+    setQuantity: (state, payload) => {
+      let item = state.cart.find((product) => {
+        return product.id === payload.id;
+      });
+      item.qty = payload.qty;
+    },
+    setPrice: (state, payload) => {
+      let item = state.cart.find((product) => {
+        return product.id === payload.id;
+      });
+      item.subtotal = payload.subtotal;
+    },
+    setCharge: (state, payload) => {
+      state.charge = payload;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
