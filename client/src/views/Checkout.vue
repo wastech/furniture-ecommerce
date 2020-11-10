3490dd9b3807982156cdb1884fc262f5ba490d09
wastech <fataiwasiu2@gmail.com>
2020-11-10 23:53:10 +0100

    add address route

---
+++ client/src/views/Checkout.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="form-group row">
+        <label
+          for="colFormLabelLg"
+          class="col-sm-2 col-form-label col-form-label-lg"
+          >Email</label
+        >
+        <div class="col-sm-10">
+          <input
+            type="email"
+            class="form-control form-control-lg"
+            id="colFormLabelLg"
+            placeholder=""
+          />
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label
+          for="colFormLabelLg"
+          class="col-sm-2 col-form-label col-form-label-lg"
+          >country</label
+        >
+        <div class="col-sm-10">
+          <input
+            type="text"
+            class="form-control form-control-lg"
+            id="colFormLabelLg"
+            placeholder=""
+          />
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label
+          for="colFormLabelLg"
+          class="col-sm-2 col-form-label col-form-label-lg"
+          >Firstname</label
+        >
+        <div class="col-sm-10">
+          <input
+            type="text"
+            class="form-control form-control-lg"
+            id="colFormLabelLg"
+            placeholder=""
+          />
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label
+          for="colFormLabelLg"
+          class="col-sm-2 col-form-label col-form-label-lg"
+          >lastname</label
+        >
+        <div class="col-sm-10">
+          <input
+            type="tezt"
+            class="form-control form-control-lg"
+            id="colFormLabelLg"
+            placeholder=""
+          />
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label
+          for="colFormLabelLg"
+          class="col-sm-2 col-form-label col-form-label-lg"
+          >Address</label
+        >
+        <div class="col-sm-10">
+          <input
+            type="text"
+            class="form-control form-control-lg"
+            id="colFormLabelLg"
+            placeholder=""
+          />
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label
+          for="colFormLabelLg"
+          class="col-sm-2 col-form-label col-form-label-lg"
+          >city</label
+        >
+        <div class="col-sm-10">
+          <input
+            type="text"
+            class="form-control form-control-lg"
+            id="colFormLabelLg"
+            placeholder=""
+          />
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label
+          for="colFormLabelLg"
+          class="col-sm-2 col-form-label col-form-label-lg"
+          >phone</label
+        >
+        <div class="col-sm-10">
+          <input
+            type="number"
+            class="form-control form-control-lg"
+            id="colFormLabelLg"
+            placeholder=""
+          />
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label
+          for="colFormLabelLg"
+          class="col-sm-2 col-form-label col-form-label-lg"
+          >zip</label
+        >
+        <div class="col-sm-10">
+          <input
+            type="number"
+            class="form-control form-control-lg"
+            id="colFormLabelLg"
+            placeholder=""
+          />
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label
+          for="colFormLabelLg"
+          class="col-sm-2 col-form-label col-form-label-lg"
+          >state</label
+        >
+        <div class="col-sm-10">
+          <input
+            type="text"
+            class="form-control form-control-lg"
+            id="colFormLabelLg"
+            placeholder=""
+          />
+        </div>
+      </div>
+      <button type="button" class="btn btn-primary btn-lg">next step</button>
+    </form>
+    
+  </div>
+</template>
