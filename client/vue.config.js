3d47f57fc002f34ac7d06abfb6b3e0f945504b52
wastech <fataiwasiu2@gmail.com>
2020-12-02 23:29:31 +0100

    deployed

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,4 +2,9 @@ module.exports = {
   devServer: {
     proxy: "https://infinite-headland-81013.herokuapp.com",
   },
+  pwa: {
+    workboxOptions: {
+      exclude: [/_redirects/],
+    },
+  },
 };
