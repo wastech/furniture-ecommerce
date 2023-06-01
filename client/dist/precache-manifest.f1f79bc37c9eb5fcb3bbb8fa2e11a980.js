5493311aa7ca5cda01314cdd0764f64d5c09c6d3
wastech <fataiwasiu2@gmail.com>
2023-06-01 01:42:35 +0100

    update

---
+++ client/dist/precache-manifest.f1f79bc37c9eb5fcb3bbb8fa2e11a980.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccc4788ac6266c7cf92",
+    "url": "/css/about.5104dea4.css"
+  },
+  {
+    "revision": "c57c630dc234a344f5a8",
+    "url": "/css/app.a8e0521f.css"
+  },
+  {
+    "revision": "35eb23143e4a731b9ca0a83273396148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ccc4788ac6266c7cf92",
+    "url": "/js/about.6c89cbaf.js"
+  },
+  {
+    "revision": "c57c630dc234a344f5a8",
+    "url": "/js/app.fbcf9f2f.js"
+  },
+  {
+    "revision": "47afe8b0c318f9fb1eae",
+    "url": "/js/chunk-vendors.2b35b6e7.js"
+  },
+  {
+    "revision": "ea6099985aeee0bc2d1a28abea61eb52",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
